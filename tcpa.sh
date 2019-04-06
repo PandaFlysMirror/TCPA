6c6e892e8878f98ec83dc5e0264a69025ee14e3c
猫语花香 <35602616+miaocloud@users.noreply.github.com>
2019-04-06 11:05:05 +0800

    Update tcpa.sh

--- tcpa.sh
+++ tcpa.sh
@@ -7,7 +7,7 @@ export LANG=en_US.UTF-8
 
 yum -y install epel-release net-tools bzip2
 
-curl -o /tmp/tcpa_packets_180619_1151.tar.bz2 https://raw.githubusercontent.com/ivmm/TCPA/master/tcpa_packets_180619_1151.tar.bz2
+curl -o /tmp/tcpa_packets_180619_1151.tar.bz2 https://raw.githubusercontent.com/miaocloud/TCPA/master/tcpa_packets_180619_1151.tar.bz2
 chmod +x /etc/rc.d/rc.local
 cat>>/etc/rc.local<<EOF
 ####tcpa####
@@ -24,7 +24,7 @@ sed -i '/####tcpa####/','/####tcpa####/d' /etc/rc.local
 ####tcpa####
 EOF
 
-rpm -ivh https://raw.githubusercontent.com/ivmm/TCPA/master/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
+rpm -ivh https://raw.githubusercontent.com/miaocloud/TCPA/master/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
 echo "内核安装完毕,3秒后将自动重启..."
 echo "重启后安装自动完成,lsmod|grep tcpa查看是否开启成功."
 sleep 3
